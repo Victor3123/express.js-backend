d62abb2a46e65b77761b3aedd82317a58f770eea
Vitya <rudvamailbox@gmail.com>
2021-10-21 13:56:51 +0300

    added bcrypt

--- firstapp/routes/auth.js
+++ firstapp/routes/auth.js
@@ -2,25 +2,24 @@ const express = require('express');
 const router = express.Router();
 const _ = require('lodash');
 const db = require('../db/db.js');
-
-
+const bcrypt = require('bcrypt');
 
 /* GET users listing. */
-router.post('/login', function ({body: {email, password}}, res, next) {
+router.post('/login', async function ({body: {email, password}}, res, next) {
     console.log(email, password);
-    db.get("SELECT * FROM users WHERE email = ? AND password = ?", [email, password], (err, row) => {
-        if (_.isUndefined(row)) {
-            res.status(404).json({
-                message: 'Wrong credentials!'
-            });
-        } else {
-            res.json({
+
+    db.get("SELECT * FROM users WHERE email = ?", email, async (err, row) => {
+        console.log(row);
+        if (!_.isUndefined(row) && await bcrypt.compare(password, row.password)) {
+            return res.json({
                 user: row,
             });
         }
 
-    })
-
+        return res.status(404).json({
+            message: 'Wrong credentials!'
+        });
+    });
 });
 
 module.exports = router;
